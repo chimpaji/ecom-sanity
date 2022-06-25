e0a8f9cbe4d532cccf86b90843922eae89debebd
chimpaji <chimpaji@outlook.com>
2022-06-25 19:14:53 +0100

    Add Layout with Footer and Navbar to the _app.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,27 @@
+import Head from 'next/head';
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
-const Layout = () => {
-  return <div>Layout</div>;
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Sanity Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
-
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar-container'>
+      <p>
+        <Link href='/'>Sanity Store</Link>
+      </p>
+      <button type='button' className='cart-icon' onClick={() => {}}>
+        <AiOutlineShoppingCart />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
